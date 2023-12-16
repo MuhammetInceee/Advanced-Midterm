d9d76db1fb00b0e807f02903bf374b7760a18487
Muhammet İNCE <muhammetinceee@gmail.com>
2023-12-16 22:18:42 +0300

    Code Refactoring
    I categorized variables based on the player's movement.

--- Assets/_Dev/Scripts/DataSO/PlayerMovementData.cs
+++ Assets/_Dev/Scripts/DataSO/PlayerMovementData.cs
@@ -1,16 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Game/Player/MovementData", fileName = "Player Movement Data")]
 public class PlayerMovementData : ScriptableObject
 {
-    [SerializeField] private float VerticalSpeed;
+    [SerializeField, FoldoutGroup("SwerveMechanic")] private float VerticalSpeed;
     public float verticalSpeed => VerticalSpeed;
     
-    [SerializeField] private float HorizontalSpeed;
+    [SerializeField, FoldoutGroup("SwerveMechanic")] private float HorizontalSpeed;
     public float horizontalSpeed => HorizontalSpeed;
     
-    [SerializeField] private float Sensitivity;
+    [SerializeField, FoldoutGroup("SwerveMechanic")] private float Sensitivity;
     public float sensitivity => Sensitivity;
 }
